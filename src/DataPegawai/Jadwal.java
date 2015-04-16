2520d4c4e6e08185fc1c07cc9153e93eb02f3823
unknown <cilvia.sianora@gmail.com>
2015-04-16 19:04:49 +0700

    add jadwal, not done yet

--- src/DataPegawai/Jadwal.java
+++ src/DataPegawai/Jadwal.java
@@ -1,5 +1,10 @@
 package DataPegawai;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,9 +18,33 @@ public class Jadwal {
 	public Jadwal(){
 		jadwalKerja = new ArrayList<>();
 	}
+
+	public List<ShiftKerja> getJadwalKerja() {
+		return jadwalKerja;
+	}
+	
+	
 	
 	public void generateJadwal(){
 		
 	}
 	
+	public void initListKaryawan(){
+		String url = "jdbc:mysql://localhost:3306/employee_management";
+		String driver = "com.mysql.jdbc.Driver";
+		String userName = "root"; 
+		String password = "";
+		try{
+			Class.forName(driver).newInstance();
+			Connection conn = DriverManager.getConnection(url,userName,password);
+			Statement st = conn.createStatement();
+			ResultSet res = st.executeQuery("Select * from shift_kerja");
+			
+			while(res.next()){
+				jadwalKerja.add(new ShiftKerja(res.getInt("id"), res.getString("jam"), res.getString("hari")));
+			}
+			conn.close();
+		}catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException e) {
+		}
+	}
 }
--- src/DataPegawai/Karyawan.java
+++ src/DataPegawai/Karyawan.java
@@ -5,5 +5,14 @@ package DataPegawai;
  * @author Felicia
  */
 public class Karyawan {
-    
+    private int id;
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
 }
