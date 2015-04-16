04fc181cdf538f1f1ada8646c15789331d160b4c
unknown <rita_miffies@yahoo.co.id>
2015-04-16 20:18:05 +0700

    query

--- src/GUI/LoginForm.java
+++ src/GUI/LoginForm.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package GUI;
 
 /**
  *
- * @author ASUS
+ * @author Rita
  */
 public class LoginForm extends javax.swing.JFrame {
 
